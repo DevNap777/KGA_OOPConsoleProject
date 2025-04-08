70e863efe8ed39dd3d5c107d96a7919938a835e4
DevNap <thdwodh789@gmail.com>
2025-04-08 22:42:40 +0900

    [Feat] 오브젝트 및 인터페이스 생성
    인터페이스를 통해 플레이어와 상호작용을 할 시 작동하도록 문 오브젝트와 그것을 상속할 오브젝트 생성

---
+++ Object.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGA_OOPConsoleProject
+{
+    /// <summary>
+    /// 게임 내에서 상호작용할 수 있는 Object 생성
+    /// 맵 내에 있어야 하므로 player와 마찬가지로 구조체를 가지고 있음.
+    /// </summary>
+    public abstract class Object
+    {
+        // Object의 위치
+        public Vector position;
+
+
+    }
+}
